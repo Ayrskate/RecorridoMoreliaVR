2b17d95181ab5751dc5762c80b13e70277aa59da
Punkid07 <ayrloncrack@hotmail.com>
2019-07-29 22:44:10 -0500

    Integración de proyecto
    Inicial Commit

---
+++ Assets/Scripts/GrabHandSC.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabHandSC : MonoBehaviour
+{
+
+    [Header("Params")]
+    public HandType handType;
+    public bool grabbing;
+    public float minTograb = 0.2f;
+
+    public enum HandType
+    {
+        Left,
+        Right
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input Manos
+        if(handType == HandType.Left)
+        {
+            if (Input.GetAxis("LeftHandTrigger") > minTograb)
+            {
+                grabbing = true;
+            }
+            else
+            {
+                grabbing = false;
+            }
+            
+        }
+        if (handType == HandType.Right)
+        {
+            if (Input.GetAxis("RightHandTrigger") > minTograb)
+            {
+                grabbing = true;
+            }
+            else
+            {
+                grabbing = false;
+            }
+        }
+
+    }
+}
