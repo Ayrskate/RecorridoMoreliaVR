6657cc4059559106879d85b636df94dd584cee23
Punkid07 <ayrloncrack@hotmail.com>
2019-07-30 01:37:53 -0500

    Avance en input
    Implementacion para poder agarrar objetos

--- Assets/Scripts/GrabHandSC.cs
+++ Assets/Scripts/GrabHandSC.cs
@@ -7,8 +7,9 @@ public class GrabHandSC : MonoBehaviour
 
     [Header("Params")]
     public HandType handType;
-    public bool grabbing;
     public float minTograb = 0.2f;
+
+    public Transform grabbableObject = null;
 
     public enum HandType
     {
@@ -16,39 +17,127 @@ public class GrabHandSC : MonoBehaviour
         Right
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private bool grabbingObject = false;
+
+    private bool _grabbing;
+    public bool Grabbing //Evento agarrar o soltar
     {
-        
+        get
+        {
+            return _grabbing;
+        }
+        set
+        {
+            bool _lastValue;
+            //Comparacion
+            if (_grabbing)
+            {
+                _lastValue = true;
+            }
+            else
+            {
+                _lastValue = false;
+            }
+                        
+            _grabbing = value;
+
+            if (_grabbing != _lastValue)
+            {
+                if (_grabbing)
+                {
+                    TakeObject();                    
+                }
+                else
+                {
+                    DropObject();
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        InputControl();
+    }
+
+    void InputControl()
+    {
         //Input Manos
-        if(handType == HandType.Left)
+        if (handType == HandType.Left)
         {
             if (Input.GetAxis("LeftHandTrigger") > minTograb)
             {
-                grabbing = true;
+                Grabbing = true;
             }
             else
             {
-                grabbing = false;
+                Grabbing = false;
             }
-            
+
         }
         if (handType == HandType.Right)
         {
             if (Input.GetAxis("RightHandTrigger") > minTograb)
             {
-                grabbing = true;
+                Grabbing = true;
             }
             else
             {
-                grabbing = false;
+                Grabbing = false;
             }
         }
+    }
 
+    void TakeObject()
+    {
+        Debug.Log("Mano " + handType.ToString() + " agarro!");
+
+        if(grabbableObject != null)
+        {
+            
+            grabbableObject.transform.parent = transform;
+            grabbableObject.transform.localPosition = Vector3.zero;
+
+            grabbingObject = true;            
+        }
+    }
+
+    void DropObject()
+    {
+        Debug.Log("Mano " + handType.ToString() + " solto!");
+
+        if (grabbableObject != null)
+        {
+            grabbableObject.transform.parent = null;
+
+            grabbingObject = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Grabbable")
+        {
+
+            Debug.Log("Puede agarrar a objeto: " + other.gameObject.name);
+
+
+            if (!grabbingObject)
+            {                
+                grabbableObject = other.transform;                
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Grabbable")
+        {
+            Debug.Log("Ya no puede agarrar a objeto: " + other.gameObject.name);
+
+            if(!grabbingObject)
+                grabbableObject = null;
+        }
     }
 }
